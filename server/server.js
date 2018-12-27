600fdf90a99d047d765f15ca750a0946814a2ba3
Mike O'Connor <m.k.oconnor53@gmail.com>
2018-12-26 18:04:03 -0600

    Updates babel and server file stucture for testing

--- server/server.js
+++ server/server.js
@@ -1,47 +1,5 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const db = require("../db/db.js");
-const bodyParser = require("body-parser");
-
-const app = express();
+const app = require("./app.js");
 const port = process.env.PORT || 3003;
-
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(bodyParser.json());
-
-app.get("/pledges/:id", (req, res) => {
-  db.select()
-    .where({ id: req.params.id })
-    .from("pledges")
-    .then(result => {
-      result[0].pledged = Number(result[0].pledged);
-      res.status(200);
-      res.json(result[0]);
-    })
-    .catch(err => {
-      console.log("There was an error getting info from DB", err);
-      res.sendStatus(500);
-    });
-});
-
-app.post("/pledges", (req, res) => {
-  db("pledges")
-    .where({ id: req.body.id })
-    .increment({
-      pledged: req.body.pledge_amount,
-      backer_count: Number(!req.body.hasBacked)
-    })
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      console.log("There was an error updating pledge amount in db: ");
-      res.sendStatus(500);
-    });
-});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
