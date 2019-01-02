e2dc35dc1bf6511e79b80da0911f2cfedd2ecf03
Mike O'Connor <m.k.oconnor53@gmail.com>
2019-01-02 11:39:50 -0600

    updates config to allow to run locally if .env deos not exist

--- server/config.js
+++ server/config.js
@@ -20,11 +20,11 @@ if (process.env.NODE_ENV === "production") {
   };
 } else {
   config = {
-    DB_HOST: process.env.DEV_DB_HOST,
-    DB_USER: process.env.DEV_DB_USER,
-    DB_PASSWORD: process.env.DEV_DB_PASSWORD,
-    DB_NAME: process.env.DEV_DB_NAME,
-    PORT: process.env.DEV_PLEDGE_PORT
+    DB_HOST: process.env.DEV_DB_HOST || "127.0.0.1",
+    DB_USER: process.env.DEV_DB_USER || "student",
+    DB_PASSWORD: process.env.DEV_DB_PASSWORD || "student",
+    DB_NAME: process.env.DEV_DB_NAME || "pledges",
+    PORT: process.env.DEV_PLEDGE_PORT || 3003
   };
 }
 
