53d5b6dbf10d5dff8d1d7382aebdac244e2f27b9
Gabriel Bodeen <gabebodeen@gmail.com>
2019-01-09 12:09:27 -0600

    Seed a given number of project names to the database.

--- seeds/bigFakeData.js
+++ seeds/bigFakeData.js
@@ -1,3 +1,6 @@
+const faker = require('faker');
+const NUM_PROJECTS = 4;
+const NUM_PLEDGES = 10;
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
@@ -5,5 +8,15 @@ exports.seed = function (knex, Promise) {
     .then(() => knex('projects').del())
     .then(() => knex.raw('ALTER TABLE projects AUTO_INCREMENT = 1'))
     .then(() => knex.raw('ALTER TABLE pledges AUTO_INCREMENT = 1'))
-    .then(() => knex('projects').insert({ project_name: 'Petsy' }));
+    .then(() => {
+      const promises = [];
+      let project_name = '';
+      for (let i = 0; i < NUM_PROJECTS; i++) {
+        project_name = faker.commerce.productName();
+        promises.push(knex('projects').insert({ project_name }));
+      }
+      return Promise.all(promises);
+    })
+    .then(() => console.log('Insertions completed'))
+    .catch(err => console.log('Error with insertions: ', err));
 };
